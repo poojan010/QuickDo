f6edbb155471afa1ce74d6fe34bb88186813df67
Poojan Bhatt <pdbhatt010@gmail.com>
2025-07-15 17:44:33 +0530

    chore: :art: adding prettier, path aliases, vs code settings

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,28 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.ios.jsx',
+          '.android.jsx',
+          '.js',
+          '.jsx',
+          '.json',
+          '.ts',
+          '.tsx',
+        ],
+        root: ['.'],
+        alias: {
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@theme': './src/theme',
+          '@app-types': './src/types',
+        },
+      },
+    ],
+  ],
 };
